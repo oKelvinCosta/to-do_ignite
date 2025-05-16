50ccfbc1f4a0428198f9e8c003107e3897badc68
Kelvin Costa <okelvincosta@gmail.com>
2025-05-16 11:30:03 -0300

    ✨ feat: Add task

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,38 @@
-import React from "react";
+import { useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import Logo from "../assets/todo-logo.svg";
 import { CirclePlus } from "lucide-react";
 
-export default function Header() {
+export default function Header({ onSubmitTask }) {
+  const [newTask, setNewTask] = useState<string>("");
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    onSubmitTask(newTask);
+    setNewTask("");
+  }
+
   return (
     <header className="relative h-[200px] mb-20 flex  item-center justify-center bg-gray-900">
       <div className="container--780 flex flex-col items-center justify-center">
         <img src={Logo} alt="To do logo" className="w-full max-w-[120px]" />
 
         <div className="container--780 flex justify-center gap-2 absolute bottom-[-24px]">
-          <form className="flex item-center gap-2 w-full">
-            <Input placeholder="Add new task" className= "p-6 bg-gray-900 border border-secondary/50 focus:border-secondary focus-visible:border-secondary"/>
-            <Button type="submit" className="h-full px-6 ">Create
-              <CirclePlus className="size-4" />
+          <form
+            onSubmit={handleSubmit}
+            className="flex item-center gap-2 w-full"
+          >
+            <Input
+              placeholder="Add new task"
+              className="p-6 bg-gray-900 border border-secondary/50 focus:border-secondary focus-visible:border-secondary"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              required
+            />
+            <Button type="submit" className="h-full px-6">
+              Create
+              <CirclePlus className="size-4 ml-2" />
             </Button>
           </form>
         </div>
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,13 +1,44 @@
 import Header from "./Header";
 import { NotebookPen } from "lucide-react";
 import TodoList from "./TodoList";
+import { useState, useEffect } from "react";
 
 export default function Todo() {
-  // const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      content: "Tarefa 1",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      content: "Tarefa 2",
+      isCompleted: true,
+    },
+    {
+      id: 3,
+      content: "Tarefa 3",
+      isCompleted: false,
+    },
+  ]);
+
+  function onSubmitTask(taskContent: string) {
+    const newTask = {
+      id: tasks.length + 1,
+      content: taskContent,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  // Monitora mudanças no estado tasks
+  useEffect(() => {
+    console.log("Tarefas atualizadas:", tasks);
+  }, [tasks]);
 
   return (
     <>
-      <Header />
+      <Header onSubmitTask={onSubmitTask} />
       <div className="container--780 mb-20">
         <div className="flex justify-between mb-4">
           <div className="flex gap-2 items-center">
